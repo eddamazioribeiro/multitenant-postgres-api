85cdf89c80b358faf8b9f3b87d33c284bd8fa5d3
Eduardo Ribeiro <ed.damazioribeiro@gmail.com>
2023-11-10 16:18:12 -0300

    Add insert to user resource

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const CLIENT_SCHEMA = process.env.CLIENT_SCHEMA;
+const knexConfig = require('../knex/knexfile');
+const knex = require('knex')(knexConfig[process.env.ENVIRONMENT]);
+
+router.post('', async (req, res) => {
+  try {
+    const user = {
+      email: req.body.email,
+      username: req.body.username
+    };
+
+    await knex('user').withSchema(CLIENT_SCHEMA).insert({ ...user });
+
+    return res.status(200).json({ data: user });
+  } catch (err) {
+    return res.status(400).json({ data: [], error: err });
+  }
+});
 
 router.get('/list', async (req, res) => {
   try {
-    const knexConfig = require('../knex/knexfile');
-    const knex = require('knex')(knexConfig[process.env.ENVIRONMENT]);
-    console.info('schema', req.query.schema);
     let schema = req.query.schema || CLIENT_SCHEMA;
   
     const result = await knex.withSchema(schema)
